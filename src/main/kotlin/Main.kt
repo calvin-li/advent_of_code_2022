239dc0d12242cdf99a9ee19aa074045ad75caede
Calvin Li <calvinliqh@gmail.com>
2022-12-02 17:56:44 -0800

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
-import kotlin.math.min
-import java.util.PriorityQueue
-
 const val filename = "input.txt"
 const val testFile = "test.txt"
 val input = Reader(if (System.getenv("TEST") == "True") testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    println(input.readLines())
+    val message: String = input.readAll()
+    val elfString = (message.split("\n\n").map { i -> i.split("\n").filter{ j -> j != ""}})
+    val elves = elfString.map { it.sumOf { i -> i.toInt() } }.sorted()
+
+    println(elves.takeLast(3).sum())
 }
