b3df47b76bf9257355c6096eddb298ae4b6cc0c2
Calvin Li <calvinliqh@gmail.com>
2022-12-04 01:05:41 -0800

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,17 @@
-import kotlin.math.min
-import java.util.PriorityQueue
-
 const val filename = "input.txt"
 const val testFile = "test.txt"
 val input = Reader(if (System.getenv("TEST") == "True") testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    println(input.readLines())
+    val containers: List<Boolean> = input.readLines().map { contain(it) }
+    println(containers.count { it })
+}
+
+fun contain(assigns: String): Boolean {
+    val sections = assigns.split(',').map { it.split('-') }.map { Pair(it[0].toInt(), it[1].toInt()) }
+    val first = sections.minBy { it.first }
+    val second = sections.maxBy { it.first }
+
+    return first.second >=second.first
 }
