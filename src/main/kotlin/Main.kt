f13851eb8d638b249b22bc2537394d8a474d4fea
Calvin Li <calvinliqh@gmail.com>
2022-12-04 00:44:45 -0800

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,5 +7,21 @@ val input = Reader(if (System.getenv("TEST") == "True") testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    println(input.readLines())
+    val scores = input.readLines().chunked(3).map { score(it) }
+
+    println(scores.sum())
+}
+
+fun score(packs: List<String>): Int {
+    val packsSet = packs.map { it.toSet() }
+
+
+    val common: Char = packsSet[0].intersect(packsSet[1]).intersect(packsSet[2]).first()
+
+    return if (common <= 'Z') {
+        common - 'A' + 27
+    }
+    else{
+        common - 'a' + 1
+    }
 }
