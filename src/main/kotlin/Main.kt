a74b8349dbbf760969e303227b307b91cdd5049d
Calvin Li <calvinliqh@gmail.com>
2022-12-17 15:18:56 -0800

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,27 @@
-import kotlin.math.min
-import java.util.PriorityQueue
-
 const val filename = "input.txt"
 const val testFile = "test.txt"
 val input = Reader(if (System.getenv("TEST") == "True") testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    println(input.readLines())
+    val buffer = input.readLine()
+    val mSize = 14
+    val window = mutableMapOf<Char, Int>()
+
+    buffer.forEachIndexed{i, c ->
+        window[c] = window[c]?.plus(1) ?: 1
+
+        if (i >= mSize){
+            if (window[buffer[i-mSize]]!! > 1){
+                window[buffer[i-mSize]] = window[buffer[i-mSize]]!! - 1
+            } else{
+                window.remove(buffer[i-mSize])
+            }
+
+            if (window.none { entry -> entry.value > 1 } ){
+                println(i+1)
+                return
+            }
+        }
+    }
 }
