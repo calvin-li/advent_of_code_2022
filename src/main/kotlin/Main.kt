0967d0d31036ad6810e705a69ef62111e88cff43
Calvin Li <calvinliqh@gmail.com>
2022-12-19 00:29:27 -0800

    solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,5 +6,34 @@ val input = Reader(if (testing) testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    println(input.readLines())
+    val ops = input.readLines()
+    val cycles = mutableListOf<Int>()
+    cycles.add(1)
+
+    ops.forEach {
+        val x = cycles.last()
+        cycles.add(x)
+        if (it.startsWith("addx")){
+            cycles.add(x + it.split(' ')[1].toInt())
+        }
+    }
+
+    val peaks = (0..5).map { it * 40 + 20 }
+
+    val crtRows = 6
+    val crtCols = 40
+    val crt = Array(crtRows){ CharArray(crtCols) }
+
+    cycles.forEachIndexed { i, _ ->
+        if (i > 0) {
+            val c = i-1
+            val x = cycles[c]
+            val h = c % crtCols
+            val pixel = if (h in (x-1..x+1)) '#' else '.'
+
+            crt[c/crtCols][h] = pixel
+        }
+    }
+
+    crt.forEach { println(((it.map{ i-> "$i$i"}.reduce{a, n -> a + n}))) }
 }
